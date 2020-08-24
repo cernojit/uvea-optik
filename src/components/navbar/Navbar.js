8974a63f345f55b687a38db9ef4fac52b37d8500
cernojit <cernojit@users.noreply.github.com>
2020-08-24 12:52:17 +0200

    modal update

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -45,12 +45,13 @@ class NavbarTachyons extends Component {
 			    <img src={Logo} id="icon" className="dib br-100" alt="UVEA optic"/>
 			  </a>
 			  
+
 			  <div id="menu" className="static-l absolute mt0 pa0-l pr4-l left-100 db dtc-l v-mid w-40 w-50-s w-100-l w-75-l h-80 tr">
-			    <a className="link tr tl-l fw6 db dib-l mr4 mr4-l mt4" href="#home" title="Home" onClick={this.hideNav}>Domů</a>
-			    <a className="link tr tl-l fw6 db dib-l mr4 mr4-l mt4" href="#sluzby" title="Služby" onClick={this.hideNav}>Služby</a>
-			    <a className="link tr tl-l fw6 db dib-l mr4 mr4-l mt4" href="#about" title="about" onClick={this.hideNav}>O nás</a>
-			    <a className="link tr tl-l fw6 db dib-l mr4 mr4-l mt4" href="#productPage" title="produkty" onClick={this.hideNav}>Produkty</a>			    
-			    <a className="link tr tl-l fw6 db dib-l mr4 mr0-l mt4 mb4-l mb4" href="#kontakt" title="Contact" onClick={this.hideNav}>Kde nás najdete?</a>
+				    <a className="link tr tl-l fw6 db dib-l mr4 mr4-l mt4" href="#home" title="Home" onClick={this.hideNav}>Domů</a>
+				    <a className="link tr tl-l fw6 db dib-l mr4 mr4-l mt4" href="#sluzby" title="Služby" onClick={this.hideNav}>Služby</a>
+				    <a className="link tr tl-l fw6 db dib-l mr4 mr4-l mt4" href="#about" title="about" onClick={this.hideNav}>O nás</a>
+				    <a className="link tr tl-l fw6 db dib-l mr4 mr4-l mt4" href="#productPage" title="produkty" onClick={this.hideNav}>Produkty</a>			    
+				    <a className="link tr tl-l fw6 db dib-l mr4 mr0-l mt4 mb4-l mb4" href="#kontakt" title="Contact" onClick={this.hideNav}>Kde nás najdete?</a>
 			  </div>
 			  <a href="#navbar" id="menu-btn" className="dtc tr v-mid dn-l w-20 pa0 pa0-ns tc" >
 			  <button type="button" className="white ba b--white bg-white" onClick={this.handleClickMenu}>
