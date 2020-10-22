819b90c5909c087bbe2a390d7976518783db55d8
cernojit <cernojit@users.noreply.github.com>
2020-10-22 12:29:36 +0200

    async function added

--- src/components/contact/Map.js
+++ src/components/contact/Map.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './map.css'
 
-const Map =  () => {
+const Map = () => {
+	
+	const [loaded, isLoaded] = useState(false)
+
+	useEffect(()=>{
+		isLoaded(true)
+	})
+
 	return(
 		<div id="map" >
-			<iframe title="map" src={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2554.0934102603933!2d14.840599915824559!3d50.19678487944114!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470bf7c788c0642b%3A0xaa58d72c2381051b!2sMasarykova%20583%2C%20289%2022%20Lys%C3%A1%20nad%20Labem!5e0!3m2!1scs!2scz!4v1593943285054!5m2!1scs!2scz"} >
+		{	
+			loaded 
+			?
+			<iframe title="map" src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2554.0934102603933!2d14.840599915824559!3d50.19678487944114!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470bf7c788c0642b%3A0xaa58d72c2381051b!2sMasarykova%20583%2C%20289%2022%20Lys%C3%A1%20nad%20Labem!5e0!3m2!1scs!2scz!4v1593943285054!5m2!1scs!2scz' >
 			</iframe>
+			: null
+		}
 		</div>
 		)
+
 }
 
 export default Map;
--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,15 +1,27 @@
-import React, { Component} from 'react';
+import React, { useEffect, useState } from 'react';
 import { FacebookProvider, Page } from 'react-facebook';
  
 const Example =  () =>  {
+		const [loaded, isLoaded] = useState(false)
+
+		useEffect(()=>{
+			isLoaded(true)
+		})
+
     return (
-      <FacebookProvider appId="2680676682144272">
-        <Page 
-        	href="https://www.facebook.com/UVEAoptik" 
-        	tabs="timeline"
-        	height="450px"
-        	 />
-      </FacebookProvider>    
+    	<FacebookProvider appId="2680676682144272">
+    	{
+    		loaded
+    		?
+		        <Page 
+		        	href="https://www.facebook.com/UVEAoptik" 
+		        	tabs="timeline"
+		        	height="450px"
+		        	 />
+    		: null
+    	}
+			</FacebookProvider>  
+        
     )
 }
 
