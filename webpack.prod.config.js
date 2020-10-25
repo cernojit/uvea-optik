88089c07436c7f32d1466a5699e87b8cbfd78f68
cernojit <cernojit@users.noreply.github.com>
2020-10-25 14:42:44 +0100

    prod webpack update

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const webpack = require('webpack');
 
 module.exports = {
     entry: {
