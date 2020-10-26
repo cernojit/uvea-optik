2505b8cc3a57aea6d6744928a92fa912268164e8
cernojit <cernojit@users.noreply.github.com>
2020-10-26 23:26:17 +0100

    added favicon ico

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -2,24 +2,16 @@ import React, { useEffect, useState } from 'react';
 import { FacebookProvider, Page } from 'react-facebook';
  
 const Example =  () =>  {
-		const [loaded, isLoaded] = useState(false)
-
-		useEffect(()=>{
-			isLoaded(true)
-		})
 
     return (
     	<FacebookProvider appId={process.env.FB_ID}>
-    	{
-    		loaded
-    		?
+    	
 		        <Page 
 		        	href="https://www.facebook.com/UVEAoptik" 
 		        	tabs="timeline"
 		        	height="450px"
 		        	 />
-    		: null
-    	}
+    	
 			</FacebookProvider>  
         
     )
