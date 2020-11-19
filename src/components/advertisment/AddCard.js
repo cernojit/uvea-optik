b8ddc822b060983648430e3c5706c06aef767089
cernojit <cernojit@users.noreply.github.com>
2020-11-19 16:07:38 +0100

    added advertisment part

--- src/components/advertisment/AddCard.js
+++ src/components/advertisment/AddCard.js
@@ -8,19 +8,19 @@ const AddCard = () => {
 	return (
 		<div className="w-100  pa4-ns ph5-ns ph1 ph2-m pt2-ns pb5-ns pb4 tc" id="advertisment">
 			<div className="flex pa4-ns pb3-ns items-center pa2 ">
-				<div className="fl mr4-ns mr3 ">
+				<div className="fl mr5-ns mr3 ">
 					<a href="https://www.essilor.cz/">
 						<img src={logo} className="w-100" />
 					</a>
 				</div>
 				<div className="fl">
-					<p className="tl fw8-ns f4-ns fw7 f6 ">Spolupracujeme se společností Essilor, která nám dodává brýlové čočky.</p>
+					<p className="tl fw6-ns f4-ns fw6 f6 ">Spolupracujeme se společností Essilor, která nám dodává brýlové čočky.</p>
 				</div>
 			</div>
 			<div className="flex pa4-ns w-100-ns items-center pb4-ns pb4">
 				<picture >
-					<source media="(min-width: 451px)" srcSet={baner} type="image/jpg" className="br4 w-100" />
-					<source media="(max-width: 450px)" srcSet={baner_small} type="image/jpg" className="w-100 br4 w-100" />
+					<source media="(min-width: 480px)" srcSet={baner} type="image/jpg" className="br4 w-100" />
+					<source media="(max-width: 479px)" srcSet={baner_small} type="image/jpg" className="w-100 br4 w-100" />
 					<img src={baner} className="br4 w-100" />
 				</picture>
 			</div>
