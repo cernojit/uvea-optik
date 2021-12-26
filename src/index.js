036f758c80dee78d310580d8a80913e1e9287286
Jitka Černohorská <cernojit@gmail.com>
2021-12-26 14:29:04 +0100

    feat(packages-update): update webpack and some code cleaning

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import '../public/favicon.ico';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import './favicon.png'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-serviceWorker.unregister();
+)
